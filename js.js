8d685392fe874ec4c2852eb5108c4bd07b47f6ef
Mick Jasinski <m.jasinski@minimalcraft.net>
2018-01-02 13:16:46 +0000

    Add applyDiscount function

--- js.js
+++ js.js
@@ -3,35 +3,40 @@ startButton.addEventListener('click', giveInfo);
 
 
 function Phone(brand, model, price, color) {
-	this.brand = brand;
-	this.model = model;
-	this.price = price;
-	this.color = color;
+    var self = this;
+    this.brand = brand;
+    this.model = model;
+    this.price = price;
+    this.color = color;
+    this.applyDiscount(cutValue) {
+        Phone.price - cutValue;
+    }
 }
 
 
 function giveInfo() {
-	var entry = prompt("Enter model of your choice (iPhone 6S, Galaxy S6, OnePlus)");
-	if(entry == "Galaxy S6") {
-		return GalaxyS6.displayInfo();
-	} else if(entry == "iPhone 6S") {
-		return iPhone6S.displayInfo();
-	} else if(entry == "OnePlus") {
-		return OnePlus.displayInfo();
-	} else {
-		alert("We cannot find this model in our records.");
-	}
+    var entry = prompt("Enter model of your choice (iPhone 6S, Galaxy S6, OnePlus)");
+    if (entry == "Galaxy S6") {
+        return galaxyS6.displayInfo();
+    } else if (entry == "iPhone 6S") {
+        return iPhone6S.displayInfo();
+    } else if (entry == "OnePlus") {
+        return onePlus.displayInfo();
+    } else {
+        alert("We cannot find this model in our records.");
+    }
 }
 
 
-var GalaxyS6 = new Phone("Samsung", "Galaxy S6", 199, "white");
+
+var galaxyS6 = new Phone("Samsung", "Galaxy S6", 199, "white");
 var iPhone6S = new Phone("Apple", "iPhone 6s", 250, "space gray");
-var OnePlus = new Phone("One", "OnePlus", 129, "black");
+var onePlus = new Phone("OnePlus", "One", 129, "black");
 
 Phone.prototype.printInfo = function() {
-	console.log(this.model + " by " + this.brand + " in " + this.color + ". Now only: $" + this.price + "!");
+    console.log(this.model + " by " + this.brand + " in " + this.color + ". Now only: $" + this.price + "!");
 }
 
 Phone.prototype.displayInfo = function() {
-	alert(this.model + " by " + this.brand + " in " + this.color + ". Now only: $" + this.price + "!");
+    alert(this.model + " by " + this.brand + " in " + this.color + ". Now only: $" + this.price + "!");
 }
